2912e03393ae9f39f4f44f3d0a11fd09ec304b0d
unknown <yipkwunleong@gmail.com>
2022-04-23 16:09:09 +0800

    update

--- ml_toolkit/pickle_extend.py
+++ ml_toolkit/pickle_extend.py
@@ -1,9 +1,9 @@
 import pickle
 
-def save_pickle(file_name,memory,save_to):
-    filehandler = open('{}/{}'.format(save_to,file_name),"wb")
+def save_pickle(memory, destination_file_name):
+    filehandler = open(destination_file_name,"wb")
     pickle.dump(memory,filehandler)
     
-def load_pickle(load_from, file_name):
-    file = open('{}/{}'.format(load_from,file_name),'rb')
+def load_pickle(destination_file_name):
+    file = open(destination_file_name,'rb')
     return pickle.load(file)
