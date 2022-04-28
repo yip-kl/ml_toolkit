edc269fa53e4a8791a7748fd79e54eae7329d317
yip-kl <85790832+yip-kl@users.noreply.github.com>
2022-04-28 11:22:47 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 export BRANCH_NAME=$1
 git init
 git remote rm origin
-git remote add origin https://gl.infratest.livibank.hk/customer-insight-customization/ml_toolkit.git
+git remote add origin https://github.com/yip-kl/ml_toolkit.git
 git branch $BRANCH_NAME
 git branch -M $BRANCH_NAME
 git add .
