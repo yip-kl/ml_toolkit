51eb6ed9c8c601fc23ad87c16b016f206b221605
yip-kl <85790832+yip-kl@users.noreply.github.com>
2022-11-05 16:57:58 +0800

    Update sklearn_graphs.py

--- ml_toolkit/sklearn_graphs.py
+++ ml_toolkit/sklearn_graphs.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from sklearn.feature_selection import RFECV
 
-def rfecv_graph(rfecv_obj):
+def rfecv_graph(rfecv_obj, max_features, min_features, step):
     """ Plot cross-validation score against number of features selected for RFECV
     
     Parameters
@@ -12,43 +12,15 @@ def rfecv_graph(rfecv_obj):
     
     print("Optimal number of features : %d" % rfecv_obj.n_features_)
 
+    iteration_list = [i for i in range(max_features, max_features-len(rfecv_obj.cv_results_['mean_test_score'])*step, -step)]
+    iteration_list.reverse()
+    
     # Plot number of features VS. cross-validation scores
     plt.figure()
     plt.xlabel("Number of features selected")
     plt.ylabel("Cross validation score")
     plt.plot(
-        range(1, len(rfecv_obj.cv_results_['mean_test_score']) + 1),
+        iteration_list,
         rfecv_obj.cv_results_['mean_test_score'],
     )
     plt.show()
-    
-def plot_roc_curve(fpr, tpr, label=None):
-    """ Plot AUC-ROC curve
-    
-    Parameters
-    ----------
-    fpr, tpr: Variables returned by sklearn.metrics.roc_curve
-    
-    """    
-    
-    plt.plot(fpr, tpr, linewidth=2, label=label)
-    plt.plot([0, 1], [0, 1], 'k--') # dashed diagonal
-    plt.axis([0, 1, 0, 1])                                    
-    plt.xlabel('False Positive Rate (Fall-Out)', fontsize=16) 
-    plt.ylabel('True Positive Rate (Recall)', fontsize=16)    
-    plt.grid(True)
-    
-def plot_precision_vs_recall(precisions, recalls):
-    """ Plot Precision-Recall curve
-    
-    Parameters
-    ----------
-    precisions, recalls: Variables returned by sklearn.metrics.precision_recall_curve
-    
-    """    
-    
-    plt.plot(recalls, precisions, "b-", linewidth=2)
-    plt.xlabel("Recall", fontsize=16)
-    plt.ylabel("Precision", fontsize=16)
-    plt.axis([0, 1, 0, 1])
-    plt.grid(True)
