a297ec653c6a49f438d2fd190c31aaf98f1ce563
yip-kl <85790832+yip-kl@users.noreply.github.com>
2022-11-05 17:48:41 +0800

    Update evaluation.py

--- ml_toolkit/evaluation.py
+++ ml_toolkit/evaluation.py
@@ -25,3 +25,23 @@ def count_bucket(bucket, score):
     proportion = [i/sum(output) for i in output]
             
     return pd.DataFrame(zip(bucket, output, proportion), columns = ['bucket', 'count', 'proportion'])
+
+def precision_recall_table(y_true, y_pred, threshold_list):
+    """
+    Input:
+    threshold_list: A list containing the threshold e.g. [0.3, 0.5, 0.7]
+    """
+    
+    precision_recall = {
+        'threshold': threshold_list,
+        'precision': [],
+        'recall': [],
+        'f1': []
+    }
+    for threshold in precision_recall['threshold']:
+        y_above_threshold = y_pred>=threshold
+        precision_recall['precision'].append(precision_score(y_true, y_above_threshold))
+        precision_recall['recall'].append(recall_score(y_true, y_above_threshold))
+        precision_recall['f1'].append(f1_score(y_true, y_above_threshold))
+
+    print(pd.DataFrame.from_dict(precision_recall))
